f87227903b2c212228bee198abd89c7b56cb593f
Aite Aigbe <aite@nabla.ooo>
2022-06-30 22:34:16 -0400

    desktop ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [value, setValue] = useState(null);
+  const [converted, setConverted] = useState(null);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+    console.log(value);
+  }
+
+  function convert() {
+    // TODO: Convert text using cipher
+    setConverted(value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img alt="Twizzy Talk logo" className='logo' src={process.env.PUBLIC_URL + '/images/LOGO.png'} />
+      <p>
+        Type, then krank it
+      </p>
+      <div className='inputFields'>
+        <textarea
+          placeholder={'Type here first...'}
+          onChange={(e) => handleChange(e)}
+        ></textarea>
+        <textarea
+          placeholder={'Click to copy...'}
+          onClick={() => {
+            navigator.clipboard.writeText(converted || 'Type something first!')
+          }}
+          value={converted}
+          readOnly
+        ></textarea>
+      </div>
+      <button
+        className='convertButton'
+        onClick={() => convert()}>
+      </button>
     </div>
   );
 }
