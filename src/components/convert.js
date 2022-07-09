0b817096f50743b2050d1424b1dd7e9d706b2b7f
Aite Aigbe <aite@nabla.ooo>
2022-07-08 23:14:24 -0400

    added sequences, contractions, spelling rules

--- src/components/convert.js
+++ src/components/convert.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import '../App.css';
-import { dictionary } from './dictionary';
 
-let vowels = [`a`, `e`, `i`, `o`, `u`];
+// Dictionary: Lowercase key is necessary
+const dictionary = {
+    and: 'an',
+    balenciaga: 'Balencï',
+    before: 'B4',
+    brotha: 'brudda',
+    brother: 'brudda',
+    call: 'call',
+    ecstasy: 'X ta C',
+    flashy: 'flashey',
+    for: '4',
+    fore: '4',
+    four: '4',
+    just: 'jus',
+    la: 'Ella',
+    life: 'lyfë',
+    like: 'lyke',
+    love: 'luv',
+    lying: 'lying',
+    million: 'mil',
+    mud: 'mudd',
+    okay: 'ok',
+    one: '1',
+    piece: 'peice',
+    sleeptalking: 'sleeptalking',
+    something: 'sum',
+    summers: 'summrs',
+    that: 'dat',
+    the: 'tha',
+    to: '2',
+    trendy: 'trëndi',
+    tried: 'trië',
+    true: 'tru',
+    twin: ['twizz', 'twizzzz', 'twizzy'],
+    two: '2',
+    war: 'WAR'
+};
+
 function Convert({ setConverted, value }) {
 
-    function char(word) {
+    // Special spelling cases
+    function spell(word) {
         if (word.toLowerCase().indexOf("'") !== -1) {
             return word.replace(/'/g, '');
         }
@@ -21,16 +58,72 @@ function Convert({ setConverted, value }) {
             return word.replace(/ea/g, 'ëa');
         } else if (word.toLowerCase().indexOf("ck") !== -1) {
             return word.replace(/ck/g, 'k');
-        } else if (word.charAt(word.length - 1) === 's') {
-            return word.replace(/.$/, 'z');
         } else {
             return word;
+        };
+    }
+
+    function spell2(string) {
+        return string.replaceAll(/((?!.*is)\w+)s/gim, `$1z`)
+            .replaceAll(/([b-df-hj-np-tv-z])e/gi, `$1ë`);
+    }
+
+    // Contractions
+    function contract(string) {
+
+        return string.replaceAll(/already know?/gi, 'ard know')
+            .replaceAll(/on the|on da/gi, 'onna');
+    }
+
+    // Check for sequence
+    function sequence(array) {
+        function hashtag(array) {
+            // TODO: Add hashtags at first or last
+            return array;
         }
+        for (let i = 0; i < array.length; i++) {
+            if (array[0]) {
+                // Add hashtags at first or last
+                array = hashtag(array);
+                // TODO: Capitalize X and Y
+                if (array[i].charAt(0).toUpperCase() === 'X' || array[i].charAt(0).toUpperCase() === 'Y') {
+                    const randIndex = Math.floor(Math.random() * hashGroup.length);
+                    array[i] = word;
+                }
+            }
+            if (array[1]) {
+                // There is more than 1 word
+                if (array[i - 1] && array[i].toLowerCase() === array[i - 1].toLowerCase()) {
+                    // Capitalize repeat
+                    array[i - 1] = array[i - 1].charAt(0).toUpperCase() + array[i - 1].slice(1)
+                    array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1)
+                } else if (array[1] && (array[0].toLowerCase() !== array[1].toLowerCase())) {
+                    // Capitalize first word 
+                    array[0] = array[0].charAt(0).toUpperCase() + array[0].slice(1)
+                }
+                if ((Number.parseInt(array[i]) !== 'NaN') && array[i + 1] && (array[i + 1].toLowerCase() === 'you' || array[i + 1].toLowerCase() === 'u')) {
+                    // Make you, U
+                    array[i + 1] = 'U'
+                }
+                if (array[i].toLowerCase() === 'live' && array[i + 1] && array[i + 1].toLowerCase() === 'high') {
+                    // Change high to hi
+                    array[i + 1] = array[i + 1].split(array[i + 1][2])[0]
+                }
+                if ((array[0] && array[1] && array[2])) {
+                    //There are more than 3 words
+                    if (array[i].toLowerCase() === 'tru' && array[i + 2] && (array[i + 2].toLowerCase() === 'you' || array[i + 2].toLowerCase() === 'u')) {
+                        array[i + 1] = 'to'
+                    }
+                }
+            }
+        }
+        return array;
     }
 
     // Function to parse 
     function decipher(string) {
         let deciphered = string;
+        deciphered = contract(deciphered);
         // Get each word and put it in an array
         deciphered = deciphered.split(' ');
         // Check each word in entrance
@@ -45,9 +138,11 @@ function Convert({ setConverted, value }) {
                     dictionary[word.toLowerCase()] ?
                         // The word exists in the dictionary
                         dictionary[word.toLowerCase()]
-                        :// The word isnt a special case and needs to go thru grammar checks 
-                        char(word)
-        ).join(' ');
+                        :// The word isnt a special case and needs to go thru spelling checks 
+                        spell(word)
+        );
+        deciphered = sequence(deciphered);
+        deciphered = spell2(deciphered.join(' '));
         setConverted(deciphered);
     }
     return <>
